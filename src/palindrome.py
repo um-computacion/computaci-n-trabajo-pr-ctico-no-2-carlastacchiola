4e46e8a41e673b844f29e3937b3c0738ccac5b9f
Carla <cm.stacchiola@alumno.um.edu.ar>
2025-04-13 17:17:07 -0300

    completando tercer issue: test para casos no palíndromos

--- src/palindrome.py
+++ src/palindrome.py
@@ -1,13 +1,4 @@
-def is_palindrome(texto):
-    texto = texto.lower() #convierte a minúscula
-    texto = texto.replace(" ", "") #quita espacios
-    return texto == texto[::-1] #da vuelta la palabra
-if __name__ == "__main__":
-    texto = input("Escriba una palabra o frase: ")
-    if is_palindrome(texto):
-        print(f'"{texto}" es un palíndromo')
-    else:
-        print(f'"{texto}" no es un palíndromo')
+
 
 
 
